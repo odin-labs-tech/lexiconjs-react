8c516cbc80ba79e13ff79a5b9a33472b961d3d51
Chadwick Maycumber <36460656+cmaycumber@users.noreply.github.com>
2023-12-28 17:31:41 -0500

    Update page

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -24,7 +24,6 @@ export default function PageOne() {
             onChange={(e) => setLanguage(e.target.value)}
             id="languages"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option selected>Choose a locale</option>
             {languages.map((language) => (
               <option key={language} value={language}>
                 {language}
