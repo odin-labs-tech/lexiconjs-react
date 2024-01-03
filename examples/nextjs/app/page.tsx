5a004e076b1a9da210dd6f0693a1e84901786554
Kyle Walsh <kylemco15@gmail.com>
2024-01-03 15:10:59 -0500

    Updated nextJS example to fix selector

--- examples/nextjs/app/page.tsx
+++ examples/nextjs/app/page.tsx
@@ -6,7 +6,7 @@ import { useState } from 'react';
 
 const languages = countries.getSupportedLanguages();
 export default function PageOne() {
-  const [language, setLanguage] = useState('en-US');
+  const [language, setLanguage] = useState('en');
   const [text, setText] = useState('How are you doing?');
   const translatedText = useDebounce(text, 500);
 
@@ -24,6 +24,9 @@ export default function PageOne() {
             onChange={(e) => setLanguage(e.target.value)}
             id="languages"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option key={language} value={language}>
+              {language}
+            </option>
             {languages.map((language) => (
               <option key={language} value={language}>
                 {language}
