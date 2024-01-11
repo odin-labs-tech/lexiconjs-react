14aefd2d498817d930f928370a7b5a21bb318eb0
Kyle Walsh <kylemco15@gmail.com>
2024-01-11 14:57:59 -0500

    Modified loading logic for children and updated cache to incorporate translationGuidance (#45)
    * Modified loading logic for children
    
    * Updated hash to incorporate translationGuidance (changes will update cache)

---
+++ .changeset/nervous-eagles-sneeze.md
@@ -0,0 +1,5 @@
+---
+'@lexiconjs/react': patch
+---
+
+Updated hash to incorporate the translationGuidance (changing it should create new translations in cache).
--- src/core/cache.ts
+++ src/core/cache.ts
@@ -1,6 +1,53 @@
 import { storage } from './storage';
 import { Language } from '../types';
 
+/** An interface to interact with our translation cache */
+export const cache = {
+  /** Stores a translation in our cache */
+  set: ({
+    language,
+    originalText,
+    context,
+    translationGuidance,
+    translatedText,
+  }: {
+    language: Language;
+    originalText: string;
+    context?: string;
+    translationGuidance?: string;
+    translatedText: string;
+  }) => {
+    /** Hash the text so we can use it as a key */
+    const hash = createHash({ translationGuidance, context, originalText });
+    // Store the translation in our cache
+    storage.set(`${language}_${hash}`, translatedText);
+  },
+  /** Retrieves a translation from our cache */
+  get: ({
+    language,
+    originalText,
+    context,
+    translationGuidance,
+  }: {
+    language: Language;
+    originalText: string;
+    context?: string;
+    translationGuidance?: string;
+  }) => {
+    /** Hash the text so we can use it as a key */
+    const hash = createHash({ translationGuidance, context, originalText });
+
+    hashString(`${translationGuidance ?? ''}${context ?? ''}${originalText}`);
+    // Retrieve the translation from our cache
+    return storage.get(`${language}_${hash}`);
+  },
+  /** Clear the cache */
+  clear: () => {
+    storage.clear();
+  },
+};
+
+// HELPERS
 /** A helper method used to hash our strings into something easily stored / checked */
 const hashString = (str: string) => {
   let hash = 0;
@@ -13,42 +60,15 @@ const hashString = (str: string) => {
   return hash.toString();
 };
 
-/** An interface to interact with our translation cache */
-export const cache = {
-  /** Stores a translation in our cache */
-  set: ({
-    language,
-    originalText,
-    context,
-    translatedText,
-  }: {
-    language: Language;
-    originalText: string;
-    context?: string;
-    translatedText: string;
-  }) => {
-    /** Hash the text so we can use it as a key */
-    const hash = hashString(context ? context + originalText : originalText);
-    // Store the translation in our cache
-    storage.set(`${language}_${hash}`, translatedText);
-  },
-  /** Retrieves a translation from our cache */
-  get: ({
-    language,
-    originalText,
-    context,
-  }: {
-    language: Language;
-    originalText: string;
-    context?: string;
-  }) => {
-    /** Hash the text so we can use it as a key */
-    const hash = hashString(context ? context + originalText : originalText);
-    // Retrieve the translation from our cache
-    return storage.get(`${language}_${hash}`);
-  },
-  /** Clear the cache */
-  clear: () => {
-    storage.clear();
-  },
+/** Create a unique hash using our provided parameters */
+const createHash = ({
+  translationGuidance,
+  context,
+  originalText,
+}: {
+  translationGuidance?: string;
+  context?: string;
+  originalText: string;
+}) => {
+  return hashString(`${translationGuidance ?? ''}${context ?? ''}${originalText}`);
 };
